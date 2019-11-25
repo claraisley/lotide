9f85d647c3662a36f3800e8665bd49bd4daf7a84
claraisley <isleyc97@gmail.com>
2019-11-25 20:06:01 +0000

    Initial commit

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    (actual !== expected);
+    console.log(`🚫🚫🚫Assertion Failed: ${actual} === ${expected}`);
+  }
+
+};
+const head = function(yourArray){
+  return yourArray.shift();
+};
+
+
+
+
+
+assertEqual(head([1, 4, 5]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
