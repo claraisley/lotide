0c44d21f423301e563409bcbdb5772cc80eebe5b
claraisley <isleyc97@gmail.com>
2019-11-27 20:06:18 +0000

    Initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,47 @@
+const findKeyByValue = function(object, value) {
+
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    (actual !== expected);
+    console.log(`🚫🚫🚫Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
